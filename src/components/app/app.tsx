a2d1a0b44962e48f8f1be134187f33c22b5fa077
SevketYALCIN <sevket.yalcin@outlook.com>
2018-09-21 15:13:51 +0900

    feat: add react/webpack boilerplate

---
+++ src/components/app/app.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import "./app.scss";
+import * as tsLogo from "./ts.png";
+
+class App extends React.Component<AppProps> {
+  render() {
+    return (
+      <div className="app">
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+
+interface AppProps {
+  title: string;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./components/app/app";
+
+ReactDOM.render(
+  <App title="React + D3" />,
+  document.getElementById("root")
+);
